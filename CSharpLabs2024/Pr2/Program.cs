3d486b8aac1bd6e70591f35ebc8f307ca2e8cb46
gal4xyit <gal4xyit@gmail.com>
2024-03-01 19:05:22 +0200

    added task1

--- CSharpLabs2024/Pr2/Program.cs
+++ CSharpLabs2024/Pr2/Program.cs
@@ -2,12 +2,43 @@
 
 namespace Pr2
 {
-    internal class Program
+    class Program
     {
+        const int N = 10 + 1;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("tests");
-            Console.ReadKey();
+            Task1();
+            //Task2();
+            //Task3();
+            //Task4();
         }
+
+        static void Task1()
+        {
+            int[] numbers = { 4, 6, 12 };
+
+            Console.Write("Numbers: [ ");
+            foreach (int elem in numbers)
+            {
+                Console.Write($"{elem} ");
+            }
+
+            Console.WriteLine("]");
+
+            Console.Write($"Numbers that are in range of [1;{N}]: [ ");
+            foreach (int elem in numbers)
+            {
+                if (elem >= 1 && elem <= N)
+                {
+                    Console.Write($"{elem} ");
+                }
+            }
+
+            Console.WriteLine("]");
+        }
+
+
     }
 }
+
